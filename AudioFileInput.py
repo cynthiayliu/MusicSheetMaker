57bd8107e5b128f7b2827c5079c2e9e1aad41ec3
Cynthia Liu <cynthia.liu2004@gmail.com>
2022-10-12 14:28:52 -0400

    Latest version with scipy library problem resolved and fourier transform code started

--- AudioFileInput.py
+++ AudioFileInput.py
@@ -9,7 +9,7 @@ class AudioFileInput:
     plt.rcParams['figure.dpi'] = 100
     plt.rcParams['figure.figsize'] = (9, 7)
 
-    sampFreq, sound = wavfile.read('MoonlightSonataExcerpt.wav')
+    sampFreq, sound = wavfile.read('piano_middle_C.wav')
     print(sound.dtype, sampFreq)
     sound = sound / 2.0 ** 15
     print(sound.shape)
@@ -17,20 +17,50 @@ class AudioFileInput:
     print(length_in_s)
     time = np.arange(sound.shape[0]) / sound.shape[0] * length_in_s
 
-    plt.plot(time[6000:7000], sound[6000:7000])
-    plt.xlabel("time, s")
-    plt.ylabel("signal, relative units")
-    plt.show()
+    # plt.subplot(2, 1, 1)
+    # plt.plot(time, sound[:, 0], 'r')
+    # plt.xlabel("time, s [left channel]")
+    # plt.ylabel("signal, relative units")
+    # plt.subplot(2, 1, 2)
+    # plt.plot(time, sound[:, 1], 'b')
+    # plt.xlabel("time, s [right channel]")
+    # plt.ylabel("signal, relative units")
+    # plt.tight_layout()
+    # plt.show()
 
-    fft_spectrum = np.fft.rfft(sound)
-    freq = np.fft.rfftfreq(sound.size, d=1. / sampFreq)
+    # plt.subplot(2, 1, 1)
+    # plt.plot(time, sound, 'r')
+    # plt.xlabel("time, s [left channel]")
+    # plt.ylabel("signal, relative units")
+    # plt.subplot(2, 1, 2)
+    # plt.plot(time, sound[:, 1], 'b')
+    # plt.xlabel("time, s [right channel]")
+    # plt.ylabel("signal, relative units")
+    # plt.tight_layout()
+    # plt.show()
+
+    # signal = sound[:, 0]
+    # plt.plot(time[6000:7000], signal[6000:7000])
+    # plt.xlabel("time, s")
+    # plt.ylabel("signal, relative units")
+    # plt.show()
+
+    signal = sound
+    # plt.plot(time[6000:7000], signal[6000:7000])
+    # plt.xlabel("time, s")
+    # plt.ylabel("signal, relative units")
+    # plt.show()
+    #
+    fft_spectrum = np.fft.rfft(signal)
+    freq = np.fft.rfftfreq(signal.size, d=1. / sampFreq)
     fft_spectrum_abs = np.abs(fft_spectrum)
-    plt.plot(freq, fft_spectrum_abs)
-    plt.xlabel("frequency, Hz")
-    plt.ylabel("Amplitude, units")
-    plt.show()
 
-    plt.plot(freq[:3000], fft_spectrum_abs[:3000])
+    # plt.plot(freq, fft_spectrum_abs)
+    # plt.xlabel("frequency, Hz")
+    # plt.ylabel("Amplitude, units")
+    # plt.show()
+
+    plt.plot(freq[:5000], fft_spectrum_abs[:5000])
     plt.xlabel("frequency, Hz")
     plt.ylabel("Amplitude, units")
     plt.show()
